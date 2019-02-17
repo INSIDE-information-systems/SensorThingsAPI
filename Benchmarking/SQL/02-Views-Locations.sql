0495fa5a67ec2bd08d3cc2c1fdcc761c8c6fcdb5
KathiSchleidt <kathi@schleidt.org>
2019-02-17 22:41:50 +0100

    cleaned

--- Benchmarking/SQL/02-Views-Locations.sql
+++ Benchmarking/SQL/02-Views-Locations.sql
@@ -3,15 +3,15 @@
 DROP MATERIALIZED VIEW sta."LOCATIONS";
 
 -- Correct order: longitude, latitude
-CREATE MATERIALIZED VIEW sta."LOCATIONS" AS 
- SELECT stat.codestation AS "ID",
-    stat.libellestation AS "DESCRIPTION",
+	 
+CREATE MATERIALIZED VIEW sta."LOCATIONS"
+AS SELECT stat.codestation AS "ID",
+    sta.clean(stat.libellestation) AS "DESCRIPTION",
     'application/vnd.geo+json'::text AS "ENCODING_TYPE",
-    ((('{"type":"Point","coordinates":['::text || stat.longitude) || ','::text) || stat.latitude) || ']}'::text AS "LOCATION",
-    st_geomfromtext(((('POINT('::text || stat.longitude) || ' '::text) || stat.latitude) || ')'::text) AS "GEOM",
-    stat.libellestation AS "NAME",
+    '{"type":"Point","coordinates":[' || stat.longitude || ',' || stat.latitude || ']}' AS "LOCATION",
+    st_geomfromtext('POINT(' || stat.longitude || ' ' || stat.latitude || ')') AS "GEOM",
+    sta.clean(stat.libellestation) AS "NAME",
     NULL::bigint AS "GEN_FOI_ID",
     NULL::text AS "PROPERTIES"
-   FROM referentiel_interne.station_full stat
-WITH DATA;
+   FROM referentiel_interne.station_full stat;	
 
