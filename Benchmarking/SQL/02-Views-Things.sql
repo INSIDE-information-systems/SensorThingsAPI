52ac34749ec6e745b5d873195e62f76cf5414e9b
KathiSchleidt <kathi@schleidt.org>
2019-02-17 22:35:55 +0100

    Update 02-Views-Things.sql

--- Benchmarking/SQL/02-Views-Things.sql
+++ Benchmarking/SQL/02-Views-Things.sql
@@ -1,12 +1,10 @@
 -- Materialized View: sta."THINGS"
-
 DROP MATERIALIZED VIEW sta."THINGS";
 
 CREATE MATERIALIZED VIEW sta."THINGS" AS 
  SELECT stat.codestation AS "ID",
-    stat.libellestation AS "DESCRIPTION",
-    sta.thg_prop(stat.codestation::text) AS "PROPERTIES",
-    stat.libellestation AS "NAME"
-   FROM referentiel_interne.station_full stat
-WITH DATA;
+    sta.clean(stat.libellestation) AS "DESCRIPTION",
+    sta.thg_prop(stat.codestation) AS "PROPERTIES",
+    sta.clean(stat.libellestation) AS "NAME"
+   FROM referentiel_interne.station_full stat;
 
