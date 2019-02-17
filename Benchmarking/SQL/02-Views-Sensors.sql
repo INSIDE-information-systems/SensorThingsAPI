b64bbbee43bedfbf321dad2952715debe486b9c2
KathiSchleidt <kathi@schleidt.org>
2019-02-17 16:46:59 +0100

    Update 02-Views-Sensors.sql

--- Benchmarking/SQL/02-Views-Sensors.sql
+++ Benchmarking/SQL/02-Views-Sensors.sql
@@ -4,18 +4,15 @@ DROP MATERIALIZED VIEW sta."SENSORS";
 
 -- TODO: id should only be from cdmethana, not insituana
 CREATE MATERIALIZED VIEW sta."SENSORS" AS 
- SELECT (lpad(base.cdmethana::text, 4, '0'::text) || base.insituana::text)::bigint AS "ID",
-    (mana.nom || ' '::text) || insitu.libelle::text AS "DESCRIPTION",
+ SELECT base.cdmethana AS "ID",
+    sta.clean(mana.nom) AS "DESCRIPTION",
     'application/vnd.geo+json'::text AS "ENCODING_TYPE",
-    (mana.nom || ' '::text) || insitu.libelle::text AS "METADATA",
-    (mana.nom || ' '::text) || insitu.libelle::text AS "NAME",
-    '{"namespace": "BRGM", "type":"INSPIRE", "responsibleParty":{"organisationName": "BRGM"}}'::text AS "PROPERTIES"
+    sta.clean(mana.nom) AS "METADATA",
+    sta.clean(mana.nom) AS "NAME",
+    '{"namespace": "Sandre", "type":"INSPIRE", "responsibleParty":{"organisationName": "Sandre"}}' AS "PROPERTIES"
    FROM sta.senbase base
-     LEFT JOIN referentiel.analyse_in_situ_laboratoire insitu ON base.insituana::text = insitu.code::text
      LEFT JOIN referentiel.methode mana ON mana.code::text = base.cdmethana::text
-  WHERE base.cdmethana IS NOT NULL
-    AND base.insituana IS NOT NULL
-WITH DATA;
+  WHERE base.cdmethana IS NOT NULL;
 
 -- Index: sta.mv_pk_sensor
 -- DROP INDEX sta.mv_pk_sensor;
