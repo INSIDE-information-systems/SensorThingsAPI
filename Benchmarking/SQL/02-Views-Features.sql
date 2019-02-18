aac2028a5d9bbf2a4777af090aa994b385046c32
KathiSchleidt <kathi@schleidt.org>
2019-02-18 22:40:27 +0100

    Update 02-Views-Features.sql

--- Benchmarking/SQL/02-Views-Features.sql
+++ Benchmarking/SQL/02-Views-Features.sql
@@ -4,18 +4,17 @@ DROP MATERIALIZED VIEW sta."FEATURES";
 
 -- Correct order: longitude, latitude
 CREATE MATERIALIZED VIEW sta."FEATURES" AS 
- SELECT (samp.cdprelevement::text || '-'::text) || samp.cdstationmesureeauxsurface::text AS "ID",
-    (((('Water Sample from '::text || sta.libellestation::text) || ' taken on '::text) || samp.dateprel) || ' at '::text) || samp.heureprel AS "DESCRIPTION",
-    (((('Water Sample from '::text || sta.libellestation::text) || ' taken on '::text) || samp.dateprel) || ' at '::text) || samp.heureprel AS "NAME",
-    'application/vnd.geo+json'::text AS "ENCODING",
-    ((('{"type":"Point","coordinates":['::text || sta.longitude) || ','::text) || sta.latitude) || ']}'::text AS "FEATURE",
-    st_geomfromtext(((('POINT('::text || sta.longitude) || ' '::text) || sta.latitude) || ')'::text) AS "GEOM",
-    ((((((((((((((('{ "purpose": "'::text || samp.finaliteprel::text) || '", "responsibleParty": { "code": "'::text) || rp.code::text) || '", "name": "'::text) || rp.nom::text) || '"}, "phenomenonTime": { "startdate": "'::text) || samp.dateprel) || samp.heureprel) || '", "enddate": "'::text) || samp.datefinprel) || samp.heurefinprel) || '"}, "sampledFeature": { "code": "'::text) || sta.codecourseau::text) || '", "name": "'::text) || sta.libellecourseau::text) || '" } }'::text AS "PROPERTIES"
-   FROM sta.sampbase samp
-     LEFT JOIN referentiel.intervenant rp ON samp.preleveur::text = rp.code::text -- added left
-     LEFT JOIN referentiel_interne.station_full sta ON samp.cdstationmesureeauxsurface::text = sta.codestation::text -- added left
-  WHERE NOT samp.cdprelevement IS NULL
-WITH DATA;
+ SELECT foi.cdprelevement || '-' || lpad(foi.cdstationmesureeauxsurface, 8, '0') AS "ID",
+    'Water Sample from ' || sta.libellestation || ' taken on ' || foi.dateprel || ' at ' || foi.heureprel AS "DESCRIPTION",
+    'Water Sample from ' || sta.libellestation || ' taken on ' || foi.dateprel || ' at ' || foi.heureprel AS "NAME",
+    'application/vnd.geo+json' AS "ENCODING",
+    '{"type":"Point","coordinates":[' || sta.longitude || ',' || sta.latitude || ']}' AS "FEATURE",
+    st_geomfromtext('POINT(' || sta.longitude || ' ' || sta.latitude || ')') AS "GEOM",
+    sta.foi_prop(foi.finaliteprel, rp.code, rp.nom, foi.dateprel::text, foi.heureprel::text, foi.datefinprel::text, foi.heurefinprel::text, sta.codecourseau, sta.libellecourseau) AS "PROPERTIES"
+    FROM sta.foibase foi
+     LEFT JOIN referentiel.intervenant rp ON foi.preleveur = rp.code -- added left
+     LEFT JOIN referentiel_interne.station_full sta ON foi.cdstationmesureeauxsurface = sta.codestation -- added left
+  WHERE NOT foi.cdprelevement IS NULL;
 
 CREATE UNIQUE INDEX mv_pk_foi
   ON sta."FEATURES"
