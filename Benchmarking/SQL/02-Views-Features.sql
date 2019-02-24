bdf6e9f5bffc17f4c3e3f15680cf67b471334fbb
KathiSchleidt <kathi@schleidt.org>
2019-02-24 14:35:51 +0100

    Update 02-Views-Features.sql

--- Benchmarking/SQL/02-Views-Features.sql
+++ Benchmarking/SQL/02-Views-Features.sql
@@ -10,8 +10,8 @@ CREATE MATERIALIZED VIEW sta."FEATURES" AS
     'application/vnd.geo+json' AS "ENCODING",
     '{"type":"Point","coordinates":[' || sta.longitude || ',' || sta.latitude || ']}' AS "FEATURE",
     st_geomfromtext('POINT(' || sta.longitude || ' ' || sta.latitude || ')') AS "GEOM",
-    sta.foi_prop(foi.finaliteprel, rp.code, rp.nom, foi.dateprel::text, foi.heureprel::text, foi.datefinprel::text, foi.heurefinprel::text, sta.codecourseau, sta.libellecourseau) AS "PROPERTIES"
-    FROM sta.foibase foi
+    sta.foi_prop(foi.finaliteprel::text, rp.code::text, rp.nom::text, foi.dateprel::text, foi.heureprel::text, foi.datefinprel::text, foi.heurefinprel::text, sta.codecourseau::text, sta.libellecourseau::text, foi.codeoperationcep::text) AS "PROPERTIES"
+   FROM sta.foibase foi
      LEFT JOIN referentiel.intervenant rp ON foi.preleveur = rp.code -- added left
      LEFT JOIN referentiel_interne.station_full sta ON foi.cdstationmesureeauxsurface = sta.codestation -- added left
   WHERE NOT foi.cdprelevement IS NULL;
