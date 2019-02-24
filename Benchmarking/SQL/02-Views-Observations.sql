96dd02103939a259b541db9422039a2d841a83f3
KathiSchleidt <kathi@schleidt.org>
2019-02-24 21:01:53 +0100

    Update 02-Views-Observations.sql

--- Benchmarking/SQL/02-Views-Observations.sql
+++ Benchmarking/SQL/02-Views-Observations.sql
@@ -3,39 +3,40 @@
 DROP MATERIALIZED VIEW sta."OBSERVATIONS";
 
 CREATE MATERIALIZED VIEW sta."OBSERVATIONS" AS 
- SELECT obs.id AS "ID",
-        CASE
-            WHEN obs.heureprel IS NULL THEN obs.dateprel
-            ELSE ((obs.dateprel || ' '::text) || obs.heureprel)::date
-        END AS "PHENOMENON_TIME_START",
-        CASE
-            WHEN obs.datefinprel IS NULL THEN 
+ SELECT obs.id::bigint AS "ID",
+		CASE
+				WHEN obs.heureprel IS NULL THEN (obs.dateprel || ' 12:00:00')::date
+				ELSE (obs.dateprel || ' ' || obs.heureprel)::date
+		END AS "PHENOMENON_TIME_START",
+		CASE
+				WHEN obs.datefinprel IS NULL THEN
 				CASE
-		            WHEN obs.heureprel IS NULL THEN obs.dateprel
-		            ELSE ((obs.dateprel || ' '::text) || obs.heureprel)::date
-		        END
-            ELSE
-            CASE
-                WHEN obs.heurefinprel IS NULL THEN obs.datefinprel
-                ELSE ((obs.datefinprel || ' '::text) || obs.heurefinprel)::date
-            END
-        END AS "PHENOMENON_TIME_END",
-        CASE
-            WHEN obs.dateana IS NULL THEN NULL::date
-            ELSE
-            CASE
-                WHEN obs.heureana IS NULL THEN obs.dateana
-                ELSE ((obs.dateana || ' '::text) || obs.heureana)::date
-            END
-        END AS "RESULT_TIME",
+						WHEN obs.heureprel IS NULL THEN (obs.dateprel || ' 12:00:00')::date
+						ELSE (obs.dateprel || ' ' || obs.heureprel)::date
+				END
+				ELSE
+				CASE
+						WHEN obs.heurefinprel IS NULL THEN (obs.datefinprel || ' 12:00:00')::date
+						ELSE (obs.datefinprel || ' ' || obs.heurefinprel)::date
+				END
+		END AS "PHENOMENON_TIME_END",				
+		CASE
+				WHEN obs.dateana IS NULL THEN NULL::date
+				ELSE
+				CASE
+						WHEN obs.heureana IS NULL THEN (obs.dateana || ' 12:00:00')::date
+						ELSE (obs.dateana || ' ' || obs.heureana)::date
+				END
+		END AS "RESULT_TIME",
     obs.rsana AS "RESULT_NUMBER",
     obs.rsana::text AS "RESULT_STRING",
-    ((((((((((('[{ "nameOfMeasure": "DQ_Status", "DQ_Result": { "code": "'::text || obs.statutana::text) || '", "label": "'::text) || stat.mnemo::text) || '", "comment": "'::text) || stat.libelle::text) || '"} }, { "nameOfMeasure": "DQ_Qualification", "DQ_Result": { "code": "'::text) || obs.qualana::text) || '", "label": "'::text) || qual.mnemo::text) || '", "comment": "'::text) || qual.libelle::text) || '" } } ]'::text AS "RESULT_QUALITY",
+    '[{ "nameOfMeasure": "DQ_Status", "DQ_Result": { "code": "http://id.eaufrance.fr/nsa/446#' || obs.statutana || '", "label": "' || stat.mnemo || '", "comment": "' || stat.libelle || '"} }, { "nameOfMeasure": "DQ_Qualification", "DQ_Result": { "code": "http://id.eaufrance.fr/nsa/414#' || obs.qualana || '", "label": "' || qual.mnemo || '", "comment": "' || qual.libelle || '" } } ]' AS "RESULT_QUALITY",
     NULL::date AS "VALID_TIME_START",
     NULL::date AS "VALID_TIME_END",
-    sta.obs_prop(obs.comresultatana, obs.commentairesana, obs.incertana::text, obs.rdtextraction::text, obs.rqana::text, com.libelle::text, obs.difficulteana::text, difana.mnemo::text, obs.difficulteprel::text, difprel.mnemo::text, obs.cdmethfractionnement::text, meth.nom, obs.cdmethodeprel::text, methprel.nom) AS "PARAMETERS",
-    (((((obs.insituana::text || obs.cdunitemesure::text) || obs.cdfractionanalysee::text) || obs.cdsupport::text) || obs.cdparametre::text) || obs.cdmethana::text) || obs.cdstationmesureeauxsurface::text AS "DATASTREAM_ID",
-    (obs.cdprelevement::text || '-'::text) || obs.cdstationmesureeauxsurface::text AS "FEATURE_ID",
+    sta.obs_param(obs.comresultatana, obs.commentairesana, obs.incertana::text, obs.rdtextraction::text, obs.rqana::text, com.mnemo::text, obs.difficulteana::text, difana.mnemo::text, obs.difficulteprel::text, difprel.mnemo::text, obs.cdmethfractionnement::text, meth.nom, obs.cdmethodeprel::text, methprel.nom, obs.insituana, ins.mnemo) AS "PARAMETERS",
+    obs.cdunitemesure || obs.cdfractionanalysee || obs.cdsupport || obs.cdparametre || obs.cdmethana || obs.cdstationmesureeauxsurface AS "DATASTREAM_ID",
+    obs.cdprelevement || '-' || obs.cdstationmesureeauxsurface AS "FEATURE_ID", 
+    obs.insituana,
     0 AS "RESULT_TYPE",
     NULL::text AS "RESULT_JSON",
     NULL::boolean AS "RESULT_BOOLEAN",
@@ -48,8 +49,8 @@ CREATE MATERIALIZED VIEW sta."OBSERVATIONS" AS
      LEFT JOIN referentiel.difficulte_prelevement difprel ON obs.difficulteana::text = difprel.code::text
      LEFT JOIN referentiel.methode meth ON obs.cdmethfractionnement::text = meth.code::text
      LEFT JOIN referentiel.methode methprel ON methprel.code::text = obs.cdmethodeprel::text
-  WHERE obs.cdprelevement IS NOT NULL
-WITH DATA;
+		 LEFT JOIN referentiel.analyse_in_situ_laboratoire ins ON obs.insituana = ins.code
+  WHERE obs.cdprelevement IS NOT null;
 
 -- Index: sta."OBS_ID"
 -- DROP INDEX sta."OBS_ID";
