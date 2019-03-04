ea5f1abf5395e0cc37cb5ba8ce58bab966f7d77e
KathiSchleidt <kathi@schleidt.org>
2019-03-04 17:16:19 +0100

    Create 02-Views-CE_Datastreams.sql

---
+++ Benchmarking/SQL/02-Views-CE_Datastreams.sql
@@ -0,0 +1,25 @@
+-- drop MATERIALIZED VIEW sta."CE_DATASTREAMS";
+
+
+CREATE MATERIALIZED VIEW sta."CE_DATASTREAMS" AS 
+ SELECT lpad(base.cdunitemesure, 5, '0') || 
+			lpad(base.cdparametre, 5, '0') || lpad(base.cdmethode, 5, '0') || lpad(base.cdstationmesureeauxsurface, 8, '0') AS "ID",
+    sta.clean(par.libellelong) || ' at ' || sta.clean(sta.libellestation) || ' with method ' || sta.clean(mana.nom) AS "DESCRIPTION",
+    'http://www.opengis.net/def/observationType/OGC-OM/2.0/OM_Measurement' AS "OBSERVATION_TYPE",
+    sta.make_time(base.mindateprel::text, base.minheureprel::text) AS "PHENOMENON_TIME_START",
+    sta.make_time(base.maxdateprel::text, base.maxheureprel::text) AS "PHENOMENON_TIME_END",
+    base.cdmethode::bigint AS "SENSOR_ID",
+    base.cdparametre::bigint AS "OBS_PROPERTY_ID",
+    base.cdstationmesureeauxsurface AS "THING_ID",
+    unite.symbole AS "UNIT_NAME",
+    unite.symbole AS "UNIT_SYMBOL",
+    'http://id.eaufrance.fr/urf/' || base.cdunitemesure AS "UNIT_DEFINITION",
+    sta.clean(par.libellelong) || ' at ' || sta.clean(sta.libellestation) || ' with method ' || sta.clean(mana.nom) AS "NAME",
+    st_geomfromtext('POINT(' || sta.latitude || ' ' || sta.longitude || ')') AS "OBSERVED_AREA",
+    null::text AS "PROPERTIES"
+   FROM sta.ce_dsbase base
+     LEFT JOIN referentiel_interne.station_full sta ON base.cdstationmesureeauxsurface = sta.codestation
+     LEFT JOIN referentiel.unite ON unite.code = base.cdunitemesure
+     LEFT JOIN referentiel.methode mana ON mana.code = base.cdmethode
+     LEFT JOIN referentiel.parametre par ON base.cdparametre = par.code
+   where not base.mindateprel is null;
