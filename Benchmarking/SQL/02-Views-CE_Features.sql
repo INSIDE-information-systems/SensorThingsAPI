31b2e228afc2ccd1e6e900530a6099a8f48ce45e
KathiSchleidt <kathi@schleidt.org>
2019-03-04 21:20:46 +0100

    Rename 02-Views-Features.sql to 02-Views-CE_Features.sql

---
+++ Benchmarking/SQL/02-Views-CE_Features.sql
@@ -0,0 +1,15 @@
+-- Materialized View: sta."CE_FEATURES"
+
+DROP MATERIALIZED VIEW sta."CE_FEATURES";
+
+-- Correct order: longitude, latitude
+CREATE MATERIALIZED VIEW sta."CE_FEATURES" AS 
+ SELECT foi.codeoperationcep AS "ID",
+    'Water Sample from ' || sta.libellestation || ' taken on ' || foi.maxdateprel || ' at ' || foi.maxheureprel AS "DESCRIPTION",
+    foi.codeoperationcep AS "NAME",
+    'application/vnd.geo+json' AS "ENCODING",
+    '{"type":"Point","coordinates":[' || sta.longitude || ',' || sta.latitude || ']}' AS "FEATURE",
+    st_geomfromtext('POINT(' || sta.longitude || ' ' || sta.latitude || ')') AS "GEOM",
+    sta.ce_foi_prop(foi.mindateprel::text, foi.minheureprel::text, foi.maxdateprel::text, foi.maxheureprel::text, sta.codecourseau::text, sta.libellecourseau::text) AS "PROPERTIES"
+   FROM sta.ce_foibase foi
+     LEFT JOIN referentiel_interne.station_full sta ON foi.cdstationmesureeauxsurface = sta.codestation;
