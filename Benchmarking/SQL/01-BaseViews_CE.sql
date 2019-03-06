6be23c941a1cd14256d80675aba21a337e670edd
KathiSchleidt <kathi@schleidt.org>
2019-03-06 20:28:47 +0100

    Update 01-BaseViews_CE.sql

--- Benchmarking/SQL/01-BaseViews_CE.sql
+++ Benchmarking/SQL/01-BaseViews_CE.sql
@@ -13,6 +13,7 @@ max(ce.heureparenv) AS maxheureprel
 FROM physicochimie.condition_environnementale ce
 WHERE NOT ce.cdprelevement is NULL
 GROUP BY ce.cdstationmesureeauxsurface, ce.cdmethode, ce.cdparametre, ce.cdunitemesure;
+
 -- base view for the creation of FEATURES
 DROP MATERIALIZED VIEW sta.ce_foibase cascade;
 
@@ -27,29 +28,3 @@ AS SELECT ce.codeoperationcep,
    WHERE NOT ce.cdprelevement is NULL
    GROUP BY ce.codeoperationcep, ce.dateprel, ce.cdstationmesureeauxsurface;
 	
--- base view for the creation of SENSORS
-DROP MATERIALIZED VIEW sta.ce_senbase cascade;
-
-CREATE MATERIALIZED VIEW sta.ce_senbase
-AS SELECT ce.cdmethode
-   FROM physicochimie.condition_environnementale ce
-   WHERE NOT ce.cdprelevement is NULL
-  GROUP BY ce.cdmethode;
-  
--- base view for the creation of OBS_PROPERTIES
-DROP MATERIALIZED VIEW sta.ce_propbase cascade;
-
-CREATE MATERIALIZED VIEW sta.ce_propbase
-AS SELECT ce.cdparametre
-   FROM physicochimie.condition_environnementale ce
-   WHERE NOT ce.cdprelevement is NULL
-GROUP BY ce.cdparametre;
-
--- base view for the creation of THINGS
-DROP MATERIALIZED VIEW sta.ce_thgbase cascade;
-
-CREATE MATERIALIZED VIEW sta.ce_thgbase
-AS SELECT ce.cdstationmesureeauxsurface 
-   FROM physicochimie.condition_environnementale ce
-   WHERE NOT ce.cdprelevement is NULL
-GROUP BY ce.cdstationmesureeauxsurface;  
