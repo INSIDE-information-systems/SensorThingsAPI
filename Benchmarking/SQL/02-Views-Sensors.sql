104cace0426594a545f390a8e83cfad90bde7be1
KathiSchleidt <kathi@schleidt.org>
2019-03-06 19:57:18 +0100

    updated to support both pc and ce

--- Benchmarking/SQL/02-Views-Sensors.sql
+++ Benchmarking/SQL/02-Views-Sensors.sql
@@ -4,15 +4,15 @@ DROP MATERIALIZED VIEW sta."SENSORS";
 
 -- TODO: id should only be from cdmethana, not insituana
 CREATE MATERIALIZED VIEW sta."SENSORS" AS 
- SELECT base.cdmethana::bigint AS "ID",
+ SELECT base.meth::bigint AS "ID",
     sta.clean(mana.nom) AS "DESCRIPTION",
     'application/vnd.geo+json'::text AS "ENCODING_TYPE",
     sta.clean(mana.nom) AS "METADATA",
     sta.clean(mana.nom) AS "NAME",
     '{"namespace": "Sandre", "type":"INSPIRE", "responsibleParty":{"organisationName": "Sandre"}}' AS "PROPERTIES"
    FROM sta.senbase base
-     LEFT JOIN referentiel.methode mana ON mana.code::text = base.cdmethana::text
-  WHERE base.cdmethana IS NOT NULL;
+     LEFT JOIN referentiel.methode mana ON mana.code::text = base.meth::text
+WHERE base.meth IS NOT NULL;
 
 -- Index: sta.mv_pk_sensor
 -- DROP INDEX sta.mv_pk_sensor;
