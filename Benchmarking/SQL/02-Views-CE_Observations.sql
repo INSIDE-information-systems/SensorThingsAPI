da329f358c47287ac0e307c4d2c020a204b0ed2b
KathiSchleidt <kathi@schleidt.org>
2019-03-07 21:59:52 +0100

    Update 02-Views-CE_Observations.sql

--- Benchmarking/SQL/02-Views-CE_Observations.sql
+++ Benchmarking/SQL/02-Views-CE_Observations.sql
@@ -1,11 +1,11 @@
 CREATE MATERIALIZED VIEW sta."CE_OBSERVATIONS"
 TABLESPACE pg_default
-AS SELECT obs.id::bigint AS "ID",
+AS SELECT sta.numeric_id_obs('CE_' || obs.id) AS "ID",
         (obs.dateprel || ' 12:00:00')::date AS "PHENOMENON_TIME_START",
         (obs.dateprel || ' 12:00:00')::date AS "PHENOMENON_TIME_END",
         CASE WHEN obs.dateparenv IS NULL 
 						THEN (obs.dateprel || ' 12:00:00')::date
-            ELSE sta.make_time(obs.dateparenv::text, obs.heureparenv::text)::date
+            ELSE sta.make_time(obs.dateparenv, obs.heureparenv)::date
         END AS "RESULT_TIME",
     obs.rsparenv AS "RESULT_NUMBER",
     obs.rsparenv::text AS "RESULT_STRING",
@@ -13,9 +13,9 @@ AS SELECT obs.id::bigint AS "ID",
     NULL::date AS "VALID_TIME_START",
     NULL::date AS "VALID_TIME_END",
     null::text AS "PARAMETERS",
-    lpad(obs.cdunitemesure, 5, '0') || 
-			lpad(obs.cdparametre, 5, '0') || lpad(obs.cdmethode, 5, '0') || lpad(obs.cdstationmesureeauxsurface, 8, '0') AS "DATASTREAM_ID",
-    obs.codeoperationcep AS "FEATURE_ID",
+   sta.numeric_id_datastream(lpad(obs.cdunitemesure, 5, '0') || 
+			lpad(obs.cdparametre, 5, '0') || lpad(obs.cdmethode, 5, '0') || lpad(obs.cdstationmesureeauxsurface, 8, '0')) AS "DATASTREAM_ID",
+    sta.numeric_id_feature('CE_' || obs.codeoperationcep) AS "FEATURE_ID",
     0 AS "RESULT_TYPE",
     NULL::text AS "RESULT_JSON",
     NULL::boolean AS "RESULT_BOOLEAN",
@@ -23,4 +23,4 @@ AS SELECT obs.id::bigint AS "ID",
    FROM physicochimie.condition_environnementale obs
      LEFT JOIN referentiel.statut_analyse stat ON obs.statutparen::text = stat.code::text
      LEFT JOIN referentiel.qualification qual ON obs.qualparenv::text = qual.code::text
-  WHERE obs.cdprelevement IS NOT NULL;	
+WHERE obs.cdprelevement IS NOT NULL; 
