da067c39d855b8b71b6d9a19232a3078d61165cb
KathiSchleidt <kathi@schleidt.org>
2019-03-07 21:16:44 +0100

    Update 02-Views-CE_Features.sql

--- Benchmarking/SQL/02-Views-CE_Features.sql
+++ Benchmarking/SQL/02-Views-CE_Features.sql
@@ -4,12 +4,12 @@ DROP MATERIALIZED VIEW sta."CE_FEATURES";
 
 -- Correct order: longitude, latitude
 CREATE MATERIALIZED VIEW sta."CE_FEATURES" AS 
- SELECT foi.codeoperationcep AS "ID",
+ SELECT sta.numeric_id_feature('CE_' || foi.codeoperationcep) AS "ID",
     'Water Sample from ' || sta.libellestation || ' taken on ' || foi.maxdateprel || ' at ' || foi.maxheureprel AS "DESCRIPTION",
     foi.codeoperationcep AS "NAME",
     'application/vnd.geo+json' AS "ENCODING",
     '{"type":"Point","coordinates":[' || sta.longitude || ',' || sta.latitude || ']}' AS "FEATURE",
     st_geomfromtext('POINT(' || sta.longitude || ' ' || sta.latitude || ')') AS "GEOM",
-    sta.ce_foi_prop(foi.mindateprel::text, foi.minheureprel::text, foi.maxdateprel::text, foi.maxheureprel::text, sta.codecourseau::text, sta.libellecourseau::text) AS "PROPERTIES"
+    sta.ce_foi_prop(foi.mindateprel, foi.minheureprel, foi.maxdateprel, foi.maxheureprel, sta.codecourseau::text, sta.libellecourseau::text) AS "PROPERTIES"
    FROM sta.ce_foibase foi
-     LEFT JOIN referentiel_interne.station_full sta ON foi.cdstationmesureeauxsurface = sta.codestation;
+LEFT JOIN referentiel_interne.station_full sta ON foi.cdstationmesureeauxsurface = sta.codestation;
