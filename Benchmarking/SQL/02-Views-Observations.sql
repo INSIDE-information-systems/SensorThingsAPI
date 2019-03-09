dc41ee64e4a2e9ffbd194b8db5f78c08a227ce3f
KathiSchleidt <kathi@schleidt.org>
2019-03-09 20:26:11 +0100

    ::timestamp with time zone

--- Benchmarking/SQL/02-Views-Observations.sql
+++ Benchmarking/SQL/02-Views-Observations.sql
@@ -12,15 +12,15 @@ CREATE MATERIALIZED VIEW sta."OBSERVATIONS" AS
 			sta.make_time(obs.datefinprel, obs.heurefinprel)
 	END AS "PHENOMENON_TIME_END",				
 	CASE
-		WHEN obs.dateana IS NULL THEN NULL
+		WHEN obs.dateana IS NULL THEN NULL::timestamp with time zone
 		ELSE
 			sta.make_time(obs.dateana, obs.heureana)
 	END AS "RESULT_TIME",
 	obs.rsana AS "RESULT_NUMBER",
 	obs.rsana::text AS "RESULT_STRING",
 	'[{ "nameOfMeasure": "DQ_Status", "DQ_Result": { "code": "http://id.eaufrance.fr/nsa/446#' || obs.statutana || '", "label": "' || stat.mnemo || '", "comment": "' || stat.libelle || '"} }, { "nameOfMeasure": "DQ_Qualification", "DQ_Result": { "code": "http://id.eaufrance.fr/nsa/414#' || obs.qualana || '", "label": "' || qual.mnemo || '", "comment": "' || qual.libelle || '" } } ]' AS "RESULT_QUALITY",
-	NULL::date AS "VALID_TIME_START",
-	NULL::date AS "VALID_TIME_END",
+	NULL::timestamp with time zone AS "VALID_TIME_START",
+	NULL::timestamp with time zone AS "VALID_TIME_END",
 	sta.obs_param(obs.comresultatana, obs.commentairesana, obs.incertana::text, obs.rdtextraction::text, obs.rqana::text, com.mnemo::text, obs.difficulteana::text, difana.mnemo::text, obs.difficulteprel::text, difprel.mnemo::text, obs.cdmethfractionnement::text, meth.nom, obs.cdmethodeprel::text, methprel.nom, obs.insituana, ins.mnemo) AS "PARAMETERS",
 	sta.numeric_id_datastream(lpad(obs.cdunitemesure, 5, '0') || lpad(obs.cdfractionanalysee, 3, '0') || lpad(obs.cdsupport, 3, '0') || 
 			lpad(obs.cdparametre, 5, '0') || lpad(obs.cdmethana, 5, '0') || lpad(obs.cdstationmesureeauxsurface, 8, '0')) AS "DATASTREAM_ID",
