27bdf23cb5e213b358eed076518e60e119fb7eec
KathiSchleidt <kathi@schleidt.org>
2019-03-12 10:19:23 +0100

    Update 02-Views-Sensors.sql

--- Benchmarking/SQL/02-Views-Sensors.sql
+++ Benchmarking/SQL/02-Views-Sensors.sql
@@ -12,7 +12,15 @@ CREATE MATERIALIZED VIEW sta."SENSORS" AS
     '{"namespace": "Sandre", "type":"INSPIRE", "responsibleParty":{"organisationName": "Sandre"}}' AS "PROPERTIES"
    FROM sta.senbase base
      LEFT JOIN referentiel.methode mana ON mana.code::text = base.meth::text
-WHERE base.meth IS NOT NULL;
+WHERE base.meth IS NOT null
+union
+select
+	9999::int8 as "ID",
+	'Unknown'::text as "DESCRIPTION",
+	'application/vnd.geo+json'::text as "ENCODING",
+	'Unknown'::text as "METADATA",
+	'Unknown'::text as "NAME",
+	null::text as "PROPERTIES";
 
 -- Index: sta.mv_pk_sensor
 -- DROP INDEX sta.mv_pk_sensor;
