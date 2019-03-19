47a571512235cb66a1cbd33c7c629d52385ca60a
KathiSchleidt <kathi@schleidt.org>
2019-03-19 14:21:56 +0100

    removed emtpy related SF

--- Benchmarking/SQL/02-Views-Features.sql
+++ Benchmarking/SQL/02-Views-Features.sql
@@ -10,10 +10,11 @@ CREATE MATERIALIZED VIEW sta."FEATURES" AS
     'application/geo+json' AS "ENCODING",
     '{"type":"Point","coordinates":[' || sta.longitude || ',' || sta.latitude || ']}' AS "FEATURE",
     st_geomfromtext('POINT(' || sta.longitude || ' ' || sta.latitude || ')') AS "GEOM",
-    sta.foi_prop(foi.finaliteprel::text, rp.code::text, rp.nom::text, foi.dateprel::text, foi.heureprel::text, foi.datefinprel::text, foi.heurefinprel::text, sta.codecourseau::text, sta.libellecourseau::text, foi.codeoperationcep::text) AS "PROPERTIES"
+    sta.foi_prop(foi.finaliteprel::text, rp.code::text, rp.nom::text, foi.dateprel::text, foi.heureprel::text, foi.datefinprel::text, foi.heurefinprel::text, sta.codecourseau::text, sta.libellecourseau::text, cefoi.codeoperationcep::text) AS "PROPERTIES"
    FROM sta.foibase foi
      LEFT JOIN referentiel.intervenant rp ON foi.preleveur = rp.code -- added left
      LEFT JOIN referentiel_interne.station_full sta ON foi.cdstationmesureeauxsurface = sta.codestation -- added left
+     left join sta.ce_foibase cefoi on foi.codeoperationcep = cefoi.codeoperationcep
   WHERE NOT foi.cdprelevement IS NULL;
 
 CREATE UNIQUE INDEX mv_pk_foi
